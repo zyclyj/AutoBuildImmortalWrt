86efb83d72adbb6af62287b294de8a6c331c0f8f
zyclyj <154661974+zyclyj@users.noreply.github.com>
2025-02-14 00:20:40 +0800

    Update build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -37,6 +37,7 @@ PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 # 增加几个必备组件 方便用户安装iStore
+PACKAGES="$PACKAGES luci-app-store"
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
